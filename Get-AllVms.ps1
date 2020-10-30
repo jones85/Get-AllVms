70be4e9877aad06689ade1799b17702a620791d2
jones85 <32510981+jones85@users.noreply.github.com>
2020-10-30 12:06:54 +0000

    Create Get-AllVms.ps1

---
+++ Get-AllVms.ps1
@@ -0,0 +1,37 @@
+function Get-AllVms {
+    <#
+       .SYNOPSIS
+            This function uncompresses the Veritas outputted .csv and imports the data
+    #>
+    [CmdletBinding()]
+    param (
+
+        [Parameter(Mandatory)]
+        [string]$Cluster,
+        [string]$sNodes,
+        [string]$oVMs
+
+    )
+
+    $oNodes = Invoke-Command -ComputerName $Cluster { 
+    
+        Get-ClusterNode | select Name
+    
+    }
+
+    [string[]]$sNodes = Foreach ($oNode in $oNodes) {
+
+        $oNode.Name
+
+    }
+
+    $sessions = New-PSSession -ComputerName $sNodes
+
+    $oVMs = Invoke-Command -Session $sessions -ScriptBlock { 
+
+       Get-VM | select Name, State
+   
+    }
+
+    $oVMs | ft Name, State
+}
